e22ea98be30e5fe261d8bf13f237fb7609a6df96
mmoua89 <mmoua.89@outlook.com>
2024-08-19 17:02:50 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+Author: Meng Moua
+Course: CSC500
+Assignment: Module 6, Portfolio Milestone 2
+"""
+
+def main():
+    pass
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
