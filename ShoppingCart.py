106e3dc901c2b2cd9f7a22c0fc67bd67ae189404
mmoua89 <mmoua.89@outlook.com>
2024-08-21 20:21:01 -0700

    Add prompt menu in main driver, Item class, and add functional per the given requirements

--- ShoppingCart.py
+++ ShoppingCart.py
@@ -3,28 +3,56 @@ A Shopping Cart class that represent a customer name,
 date and the added items with their prices and the total cost.
 """
 from datetime import datetime
+from Item import Item
 class ShoppingCart:
-    def __init__(self, customerName="none", date=datetime(2010, 12,1 )):
+    def __init__(self, customerName="none", currentDate=datetime(2020, 12, 1)):
         self.customerName = customerName
-        self.datetime = datetime
+        self.currentDate = currentDate
+        self.cartItems = []
 
-    def addItem(self, item):
-        pass
+    def addItem(self, itemToPurchase):
+        self.cartItems.append(Item(**itemToPurchase))
 
-    def removeItem(self, item):
-        pass
+    def removeItem(self, itemName):
+        for item in self.cartItems:
+            if itemName == item.itemName:
+                self.cartItems.remove(item)
+                return
+        print('Item not found in cart. Nothing removed.')
 
-    def modifyItem(self):
-        pass
+    def modifyItem(self, itemToPurchase):
+        for item in self.cartItems:
+            itemInstance = Item(**itemToPurchase)
+            if item.itemName == itemInstance.itemName:
+                if itemInstance.itemDescription != "none":
+                    item.itemDescription = itemInstance.itemDescription
+                if itemInstance.itemPrice != 0:
+                    item.itemPrice = itemInstance.itemPrice
+                return
+        print('Item not found in cart. Nothing modified.')
 
     def getNumItemInCart(self):
-        pass
+        totalQuantity = 0
+        for item in self.cartItems:
+            totalQuantity += item.itemQuantity
+        return totalQuantity
 
     def getTotalCost(self):
-        pass
+        totalCost = 0.0
+        for item in self.cartItems:
+            totalCost += item.calculate_total_cost()
+        return totalCost
 
     def printTotal(self):
-        pass
+        if len(self.cartItems) == 0:
+            print('SHOPPING CART IS EMPTY.')
+            return
+
+        for item in self.cartItems:
+            item.print_item_cost()
+
+        print('Total: {:.2f}'.format(self.getTotalCost()))
+
 
     def printDescriptions(self):
         pass
--- main.py
+++ main.py
@@ -3,9 +3,45 @@ Author: Meng Moua
 Course: CSC500
 Assignment: Module 6, Portfolio Milestone 2
 """
-
+from ShoppingCart import ShoppingCart
+from Item import Item
 def main():
-    pass
+    shoppingCart = ShoppingCart()
+    userInput = ""
+
+    while userInput != 'q' or userInput != 'quit':
+        printMenu()
+        userInput = input()
+
+        if userInput == 'a':
+            itemName = input('Item name:\n')
+            itemPrice = input('Item price:\n')
+            itemQuantity = input('Item quantity:\n')
+            itemDescription = input('Item description:\n')
+
+            # valid item_price and item_quantity
+            # if not valid_numbers(itemPrice, itemQuantity):
+            #     return
+
+            # append item in (Item) class
+            item = Item(itemName, float(itemPrice), int(itemQuantity), itemDescription)
+            shoppingCart.addItem(item)
+        elif userInput == 'o':
+            shoppingCart.printTotal()
+
+
+
+def printMenu():
+    menuList = ['a - Add item to cart',
+                'r - Remove item from cart',
+                'c - Change item quantity',
+                "i - Output items' description",
+                'o - Output shopping cart',
+                'q - quit']
+    print('Menu')
+    for menuItem in menuList:
+        print(menuItem)
+    print('Choose an option:\n')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
